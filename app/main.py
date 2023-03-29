ecefee03de01444d080f13d8b8bd274b96cbed56
喵不嗷 <617802189@qq.com>
2023-03-29 08:33:00 +0800

    feat: 添加测试域名

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ origins = [
     'http://127.0.0.1:5173', "http://localhost:5173",
     'http://127.0.0.1:9000', "http://localhost:9000",
     "https://ita.idim.cc", "http://ita.idim.cc",
+    "https://idim.cc"
 ]
 
 app.add_middleware(
