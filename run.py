1255d9d3126c317ebf259f7eb9f876e6a8c01f40
miaobuao <miaobuao@outlook.com>
2023-07-21 15:12:44 +0800

    feat: 启动脚本允许开开启生产环境

--- run.py
+++ run.py
@@ -1,11 +1,23 @@
 import uvicorn
+from argparse import ArgumentParser
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "app.main:app",
-        host='127.0.0.1',
-        port=8000, 
-        reload=True, 
-        ssl_keyfile="./localhost+1-key.pem", 
-        ssl_certfile="./localhost+1.pem"
-    )
+    p = ArgumentParser()
+    p.add_argument("--dev", action="store_true")
+    argv = p.parse_args()
+    if argv.dev:
+        uvicorn.run(
+            "app.main:app",
+            host='127.0.0.1',
+            port=8000, 
+            reload=True, 
+            ssl_keyfile="./localhost+1-key.pem", 
+            ssl_certfile="./localhost+1.pem"
+        )
+    else:
+        uvicorn.run(
+            "app.main:app",
+            host='127.0.0.1',
+            port=8012, 
+            reload=True, 
+        )
